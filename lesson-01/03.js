d74272a734ae598834e07f7a3c3b4b4b72c16683
vzw83 <vzw83@yandex.ru>
2024-01-04 12:51:20 +0300

    number++

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -10,11 +10,7 @@
 
 let number = 1
 
-console.log(number)
-number++
-number++
-console.log(number)
-console.log(number)
-number--
-number--
-console.log(number)
+console.log(number++)
+console.log(++number)
+console.log(number--)
+console.log(--number)
