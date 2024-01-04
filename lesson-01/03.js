f34a740760f8ca607c15ec456e09d5658b25b759
vzw83 <vzw83@yandex.ru>
2024-01-04 12:45:46 +0300

    number++

---
+++ lesson-01/03.js
@@ -0,0 +1,20 @@
+// Имея в распоряжении только:
+
+// ​let number = 1;
+// 📋
+// и используя лишь
+
+// --number   number--  ++number   number++
+// 📋
+// Получи в консоли 1,3,3,1 
+
+let number = 1
+
+console.log(number)
+number++
+number++
+console.log(number)
+console.log(number)
+number--
+number--
+console.log(number)
