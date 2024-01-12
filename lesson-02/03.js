69ec5de81d163b2199eedce5e986763b89bcb9f9
vzw83 <vzw83@yandex.ru>
2024-01-12 12:05:37 +0300

    test3 || &&

---
+++ lesson-02/03.js
@@ -0,0 +1,10 @@
+// Передай такие числа, что бы у нас выдало надпись 'Something wrong!'
+
+const giveMeRightNumbers = (value1, value2, maximum) => {
+    if (value1 === value2 || value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+giveMeRightNumbers(300, 100, 200)
